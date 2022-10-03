b749d4b8f043432c6e2c417fdfcdaccade34d38f
Luana Portezan <luanaportezan@gmail.com>
2022-10-03 17:01:23 -0300

    Criando as rotas necessárias para a aplicação

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Routes from './Routes';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <Routes />
+      </>
+    );
   }
 }
 
 export default App;
- 
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <p>Pagina não encontrada</p>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
