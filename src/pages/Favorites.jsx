c009db1cfde2b62214acb0acff2aaf461e54bd70
Luana Portezan <luanaportezan@gmail.com>
2022-10-03 18:17:11 -0300

    Criando um componente de cabeçalho

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class Favorites extends Component {
   render() {
     return (
-      <div data-testid="page-favorites" />
+      <div data-testid="page-favorites">
+        <Header />
+        Seu Favoritos
+      </div>
     );
   }
 }
