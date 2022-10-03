e97f9d0ed0d3431bf0503da9e258800fb44441e0
Luana Portezan <luanaportezan@gmail.com>
2022-10-03 19:24:24 -0300

    Criando um formulário para pesquisar os artistas

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,14 +2,56 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+      btnDisable: true,
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    // console.log(value);
+    this.setState(() => ({
+      [name]: value,
+    }), this.validitySearch());
+  };
+
+  validitySearch = () => {
+    const { search } = this.state;
+    const minLength = 1;
+    if (search.length >= minLength) {
+      this.setState({ btnDisable: false });
+    } else {
+      this.setState({ btnDisable: true });
+    }
+  };
+
   render() {
+    const { search, btnDisable } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <p>Buscar</p>
+        <form action="">
+          <input
+            data-testid="search-artist-input"
+            name="search"
+            type="text"
+            onChange={ this.handleChange }
+            value={ search }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ btnDisable }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
 }
-
 export default Search;
